47b13b526b86d0bea8ff8746d0486e4a60cc3ae0
Fletcher Easton <fletcher.easton@radai.com>
2024-07-27 22:18:30 -0700

    Added the minimalist package, remove the package-lock.json(s)

---
+++ minimalist/src/main.test.ts
@@ -0,0 +1,15 @@
+import { describe, test } from "vitest";
+
+import * as reporting from "@rad-ai/reporting-extensions";
+
+describe("main", () => {
+  test("test", async () => {
+    // Initialize the SDK
+    reporting.mocks.initialize();
+
+    // Import our script
+    await import("./main.js");
+
+    // Add your test code here
+  });
+});
---
+++ minimalist/src/main.ts
@@ -0,0 +1,5 @@
+import * as reporting from "@rad-ai/reporting-extensions";
+
+reporting.lifecycle.addEventListener("report-created", () => {
+  // Add your event handler here
+});
